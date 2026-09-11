f581549e61c661e3e5cd7ad0b45f96fb980cbf94
lucasfnandos <lucasfsoares@hotmail.com>
2026-09-11 20:47:18 -0300

    feat: created user repository

---
+++ src/repositories/User.ts
@@ -0,0 +1,24 @@
+import { DataSource, Repository } from "typeorm";
+import { Usuario, UsuarioRole } from "../entities/User";
+
+export class UsuarioRepository {
+  private ormRepository: Repository<Usuario>;
+  constructor(dataSource: DataSource) {
+    this.ormRepository = dataSource.getRepository(Usuario);
+  }
+
+  async create(nome: string, email: string, role: UsuarioRole, senhaHash: string): Promise<Usuario> {
+    const usuario = this.ormRepository.create({
+        nome,
+        email,
+        senha: senhaHash,
+        role: role
+    })
+    
+    return await this.ormRepository.save(usuario);
+  }
+
+  async findById(id: string): Promise<Usuario | null> {
+    return await this.ormRepository.findOneBy({ id: id });
+  }
+}
