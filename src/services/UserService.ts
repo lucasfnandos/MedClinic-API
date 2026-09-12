7cbda3292f88b4f662415a7997341b610674efbd
lucasfnandos <lucasfsoares@hotmail.com>
2026-09-12 11:32:16 -0300

    update(service/user): update create() params to reiceive dto

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,25 +1,26 @@
-import { UsuarioRepository } from "../repositories/UserRepository";
+import { UserRepository } from "../repositories/UserRepository";
 import { UsuarioMapper } from "../mappers/UsuarioMapper";
 import { UsuarioRole } from "../entities/User";
 import { hashPassword } from "../utils/hashPassword";
-import { UsuarioDto } from "../dtos/UsuarioDto";
+import { UserDto } from "../dtos/UserDto";
+import { CreateUserDto } from "../dtos/CreateUserDto";
 import { AppError } from "../types/AppError";
 
-export class UsuarioService {
-  constructor(private repo: UsuarioRepository) {}
+export class UserService {
+  constructor(private repo: UserRepository) {}
 
-  async criarUsuario(nome: string, email: string, role: UsuarioRole, senhaAberta: string): Promise<UsuarioDto | null> {
-    const emailExiste = await this.repo.findByEmail(email);
+  async createUser(userInfo: CreateUserDto): Promise<UserDto | null> {
+    const emailExists = await this.repo.findByEmail(userInfo.email);
 
-    if(emailExiste) {
+    if(emailExists) {
         throw new AppError('Email já cadastrado!', 409);
     }
-    const senhaHash = await hashPassword(senhaAberta);
-    const usuarioSalvo = await this.repo.create(nome, email, role, senhaHash);
-    return UsuarioMapper.toDto(usuarioSalvo);
+    const passHash = await hashPassword(userInfo.pwd);
+    const userSaved = await this.repo.create(userInfo);
+    return UsuarioMapper.toDto(userSaved);
   }
 
-  async listarUsuario(id: string): Promise<UsuarioDto | null> {
+  async listUser(id: string): Promise<UserDto | null> {
     const usuario = await this.repo.findById(id);
     if(!usuario) {
         throw new AppError('Usuario não encontrado', 400);
