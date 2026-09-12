187dd398a9a2fdbdc6bce58c4604cba22a12194f
lucasfnandos <lucasfsoares@hotmail.com>
2026-09-12 18:08:55 -0300

    feat: created AuthService.ts

---
+++ src/services/AuthService.ts
@@ -0,0 +1,28 @@
+import { UsuarioMapper } from "../mappers/UsuarioMapper";
+import { UserRepository } from "../repositories/UserRepository";
+import { AppError } from "../types/AppError";
+
+
+export class AuthService {
+    constructor(private repo: UserRepository) {}
+
+    async login(loginInfo: LoginDto): Promise<LoginResponseDto> {
+        const email = loginInfo.email
+        const user = await this.repo.findByEmail(email);
+        if(!user) {
+            throw new AppError("Credenciais inválidas!", 401);
+        }
+        const pwdMatch = await comparePassword(loginInfo.pwd, user.senha);
+        if(!pwdMatch) {
+            throw new AppError("Credenciais inválidas!", 401);
+        }
+        const token = tokenBuilder({ sub: user.id, role: user.role});
+
+        return {
+            token,
+            user: UsuarioMapper.toDto(user)
+        };
+
+
+    }
+}
