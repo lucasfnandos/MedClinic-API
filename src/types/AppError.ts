c467768114645fb49a5ec4671a3a2c840aaa93c2
lucasfnandos <lucasfsoares@hotmail.com>
2026-09-11 21:57:53 -0300

    feat(type): created AppError.ts in types/

---
+++ src/types/AppError.ts
@@ -0,0 +1,10 @@
+
+export class AppError extends Error {
+    constructor(
+        public readonly message: string,
+        public readonly statusCode: number = 400
+    ) {
+        super(message);
+        Object.setPrototypeOf(this, AppError.prototype);
+    }
+}
