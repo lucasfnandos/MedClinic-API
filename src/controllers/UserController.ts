66be881a849ca09fbe3d6067e9704490768756fb
lucasfnandos <lucasfsoares@hotmail.com>
2026-09-12 11:01:46 -0300

    feat: created UserController.ts

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { UserService } from "../services/UserService";
+import { CreateUserDto } from "../dtos/CreateUserDto";
+import { AppError } from "../types/AppError";
+
+export class UserController {
+    constructor(private service: UserService) {}
+
+    async create(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try{
+            const createUserDto = req.body as CreateUserDto;
+
+            const createUser = await this.service.criarUsuario(createUserDto);
+
+            return res.status(201).json(createUser);
+            
+        } catch(err){
+            next(err);
+        };
+    }
+
+    async userProfile(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try {
+            if(!req.usuario) {
+                new AppError("Usuário não autenticado", 403);
+            }
+            const user = await this.service.listarUsuario(req.usuario.sub)
+            return res.status(200).json(user);
+        } catch(err) {
+            next(err);
+        }
+    }
+}
